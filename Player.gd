3adad70de5ccf6effec7d9f17bfc5bc3566dedf7
jacobjameskbb <jacob.james.kbb@gmail.com>
2022-09-21 11:34:04 -0700

    Set up Project

---
+++ Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+### Player ###
+
+# Should deal with managing anything related to player
+# player health, player movement, player collision, player animations, ect.
+
+# Attached to this node is a collision shape and a sprite. 
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
