a35c214d8531432b5b6f489465f1e35882f56804
ZachEkstrand <zacharias_e@icloud.com>
2022-10-31 16:36:32 -0700

    added quit functionality to titlescreen

--- title_screen/title_screen.gd
+++ title_screen/title_screen.gd
@@ -1,8 +1,7 @@
 extends Control
 
 func _ready():
-	for button in $Buttons.get_children():
-		button.connect("pressed",self,"_on_Button_pressed",[button.scene_to_load])
+	$Buttons/StartButton.connect("pressed",self,"_on_Button_pressed",[$Buttons/StartButton.scene_to_load])
 
 func _on_Button_pressed(scene_to_load):
 	get_tree().change_scene(scene_to_load)
