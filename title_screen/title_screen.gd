82dba0428a24aec79eff20c08fb632771eec4159
ZachEkstrand <zacharias_e@icloud.com>
2022-11-01 01:05:29 -0700

    changed Buttons to TextureButtons

--- title_screen/title_screen.gd
+++ title_screen/title_screen.gd
@@ -1,7 +1,4 @@
 extends Control
 
-func _ready():
-	$Buttons/StartButton.connect("pressed",self,"_on_Button_pressed",[$Buttons/StartButton.scene_to_load])
-
-func _on_Button_pressed(scene_to_load):
-	get_tree().change_scene(scene_to_load)
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Main.tscn")
