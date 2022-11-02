387892e13346175fc1065e8133dee05678dedda5
ZachEkstrand <zacharias_e@icloud.com>
2022-11-01 23:39:16 -0700

    started making intro cutscene

--- Scenes/FadeToBlack.gd
+++ Scenes/FadeToBlack.gd
@@ -1,15 +1,9 @@
 extends ColorRect
 
+signal fade_finished
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func fadeToBlack():
+	$AnimationPlayer.play("FadeToBlack")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$FadeToBlack.hide()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_AnimationPlayer_animation_finished(FadeToBlack):
+	emit_signal("fade_finished")
--- title_screen/title_screen.gd
+++ title_screen/title_screen.gd
@@ -1,4 +1,9 @@
 extends Control
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://Main.tscn")
+	$FadeToBlack.show()
+	$FadeToBlack.fadeToBlack()
+
+
+func _on_FadeToBlack_fade_finished():
+	get_tree().change_scene("res://Scenes/Intro.tscn")
