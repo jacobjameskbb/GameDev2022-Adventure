4ea14800444434a0dfa18c9eb543f1cbe0c68dd6
ZachEkstrand <zacharias_e@icloud.com>
2022-11-10 11:11:08 -0800

    fixed black slide

--- Scripts/WorldManager.gd
+++ Scripts/WorldManager.gd
@@ -10,10 +10,13 @@ func _ready():
 	$BlackSplitRight.show()
 	$BlackSplitLeft/AnimationPlayer.play("BlackSplitLeft")
 	$BlackSplitRight/AnimationPlayer.play("BlackSplitRight")
-	$BlackSplitLeft.hide()
-	$BlackSplitRight.hide()
 
 func _process(_delta):
 	for node in self.get_children():
 		if node.is_in_group('enemy'):
 			node.target = $Player.position
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$BlackSplitLeft.hide()
+	$BlackSplitRight.hide()
