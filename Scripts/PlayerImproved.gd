949882011a865a485d6f80d7cedb5b8b55519ad4
ZachEkstrand <zacharias_e@icloud.com>
2022-11-19 16:17:09 -0800

    re-added animations

--- Scripts/PlayerImproved.gd
+++ Scripts/PlayerImproved.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export var speed := 150.0
+var facing
 
 func _physics_process(_delta: float) -> void:
 	var input_vector := Vector2(
@@ -11,3 +12,27 @@ func _physics_process(_delta: float) -> void:
 	var move_direction := input_vector.normalized()
 	move_and_slide(speed*move_direction)
 	print(move_direction)
+	if move_direction==Vector2(0,0):
+		$AnimatedSprite.stop()
+		if facing=="d":
+			$AnimatedSprite.animation = "idle_down"
+		if facing=="u":
+			$AnimatedSprite.animation = "idle_up"
+		if facing=="r":
+			$AnimatedSprite.animation = "idle_right"
+		if facing=="l":
+			$AnimatedSprite.animation = "idle_left"
+	if move_direction==Vector2(0,1):
+		$AnimatedSprite.animation = "walk_down"
+		facing="d"
+	if move_direction==Vector2(0,-1):
+		$AnimatedSprite.animation = "walk_up"
+		facing="u"
+	if move_direction==Vector2(1,0):
+		$AnimatedSprite.animation = "walk_right"
+		facing="r"
+	if move_direction==Vector2(-1,0):
+		$AnimatedSprite.animation = "walk_left"
+		facing="l"
+	print(facing)
+	$AnimatedSprite.play()
