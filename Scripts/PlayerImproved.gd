77ce33c461c4943cbe331ae5bd5d58c68d1f79dc
ZachEkstrand <zacharias_e@icloud.com>
2022-11-20 10:28:20 -0800

    added attacking down

--- Scripts/PlayerImproved.gd
+++ Scripts/PlayerImproved.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 export var speed := 150.0
-var facing
+var facing = "d"
 var isAttacking = false
+var savedAni= "idle_down"
 #movement
 func _physics_process(_delta: float) -> void:
 	var input_vector := Vector2(
@@ -9,48 +10,42 @@ func _physics_process(_delta: float) -> void:
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
 	var move_direction := input_vector.normalized()
-	move_and_slide(speed*move_direction)
+	if isAttacking==false:
+		move_and_slide(speed*move_direction)
 	#movement animations
-	if move_direction==Vector2(0,0):
-		$AnimatedSprite.stop()
-		if facing=="d":
-			$AnimatedSprite.play("idle_down")
-		if facing=="u":
-			$AnimatedSprite.play("idle_up")
-		if facing=="r":
-			$AnimatedSprite.play("idle_right")
-		if facing=="l":
-			$AnimatedSprite.play("idle_left")
-	if move_direction==Vector2(0,1):
-		$AnimatedSprite.play("walk_down")
-		facing="d"
-	if move_direction==Vector2(0,-1):
-		$AnimatedSprite.play("walk_up")
-		facing="u"
-	if move_direction==Vector2(1,0):
-		$AnimatedSprite.play("walk_right")
-		facing="r"
-	if move_direction==Vector2(-1,0):
-		$AnimatedSprite.play("walk_left")
-		facing="l"
+	if isAttacking==false:
+		if move_direction==Vector2(0,0):
+			$AnimatedSprite.stop()
+			if facing=="d":
+				$AnimatedSprite.play("idle_down")
+			if facing=="u":
+				$AnimatedSprite.play("idle_up")
+			if facing=="r":
+				$AnimatedSprite.play("idle_right")
+			if facing=="l":
+				$AnimatedSprite.play("idle_left")
+		if move_direction==Vector2(0,1):
+			$AnimatedSprite.play("walk_down")
+			facing="d"
+		if move_direction==Vector2(0,-1):
+			$AnimatedSprite.play("walk_up")
+			facing="u"
+		if move_direction==Vector2(1,0):
+			$AnimatedSprite.play("walk_right")
+			facing="r"
+		if move_direction==Vector2(-1,0):
+			$AnimatedSprite.play("walk_left")
+			facing="l"
 	#attacking
 	if Input.is_action_just_pressed("attack"):
-		isAttacking=true
-		move_direction=Vector2(0,0)
-		$AnimatedSprite.play("punch_down_lv.3")
+		savedAni=$AnimatedSprite.animation
+		if facing=="d":
+			$AnimatedSprite.play("punch_down_lv.3")
+			isAttacking=true
+			$AttackAreaDown/CollisionShape2D.disabled=false
 	print(move_direction,facing,isAttacking)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "punch_down_lv.3":
-		isAttacking=false;
+		$AttackAreaDown/CollisionShape2D.disabled=true
+		$AnimatedSprite.play(savedAni)
+		isAttacking=false
