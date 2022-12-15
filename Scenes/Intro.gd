1a43ba14cd7553fad0fcb748250562386a7dc101
NotBackslash <inchalik.jamie@gmail.com>
2022-12-15 11:36:32 -0800

    Demon king
    GO FORTH AND SAVE OUR KINGDOM FROM THIS CALAMITY

--- Scenes/Intro.gd
+++ Scenes/Intro.gd
@@ -9,6 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ColorRect/AnimationPlayer.play("FadeFromBlack")
+	$AudioStreamPlayer2D.play()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
