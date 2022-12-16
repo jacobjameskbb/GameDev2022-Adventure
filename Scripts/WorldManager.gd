38f271e4ece703ce110674c188df19e0f9a06eb6
jacobjameskbb <jacob.james.kbb@gmail.com>
2022-12-15 18:33:20 -0800

    Game Ending

--- Scripts/WorldManager.gd
+++ Scripts/WorldManager.gd
@@ -5,6 +5,8 @@ extends Node2D
 # World manager should control everything that happens in-game
 # World manager should contain things such as the player, the environment, the enemies, etc.
 
+var current_gummy_worms = 0
+
 func _ready():
 	$BlackSplitLeft.show()
 	$BlackSplitRight.show()
@@ -19,7 +21,13 @@ func _process(_delta):
 	if Input.is_action_just_pressed("F11"):
 		OS.window_fullscreen = !OS.window_fullscreen
 	
-		
+	current_gummy_worms = 0
+	for child in self.get_children():
+		if child.is_in_group("Enemy"):
+			current_gummy_worms += 1
+			
+	if current_gummy_worms == 0:
+		print('YOU WIN')
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	$BlackSplitLeft.hide()
